3846fe59c94ff62b1870453befde62212ec4e8d4
Cold Yelena <cyelena@vo-k7.msk.21-school.ru>
2021-10-10 16:23:01 +0300

    1

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: cyelena <cyelena@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 16:52:38 by cyelena           #+#    #+#             */
-/*   Updated: 2021/10/09 16:56:29 by cyelena          ###   ########.fr       */
+/*   Updated: 2021/10/10 16:20:19 by cyelena          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t			i;
 
-	i = 0;
 	if (dest > src)
 		ft_memcpy(dest, src, n);
 	else
 	{
+		i = 0;
 		while (i < n)
 		{
 			((char *)dest)[i] = ((char *)src)[i];
