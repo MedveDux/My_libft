06a47b13667bb55ff86aa1a37d5a840ea67c60a6
Cold Yelena <cyelena@at-r1.msk.21-school.ru>
2021-10-13 14:15:50 +0300

    4

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cyelena <cyelena@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 10:46:24 by cyelena           #+#    #+#             */
-/*   Updated: 2021/10/12 20:16:59 by cyelena          ###   ########.fr       */
+/*   Updated: 2021/10/13 13:50:31 by cyelena          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,9 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 int		ft_atoi(const char *nptr);
 #endif
